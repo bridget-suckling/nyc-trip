c7c1f575e3b5b7f2ac5bb93949e5d2e1431ebfe6
Bridget Suckling <111847401+bridget-suckling@users.noreply.github.com>
2022-11-13 09:47:34 +1300

    add post and delete routers

--- server/db/routes/activities.js
+++ server/db/routes/activities.js
@@ -26,6 +26,25 @@ router.post('/', (req, res) => {
     })
 })
 
-// router.delete('/', (req, res) => {
-//   const
-// })
+router.delete('/', (req, res) => {
+  const id = req.body.id
+  db.deleteActivity(id)
+    .then(() => {
+      res.json('deleted')
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
+
+router.patch('/:id', (req, res) => {
+  const activity = req.body
+  const id = req.params.id
+  db.updateActivity(id, activity)
+    .then(() => {
+      res.json('updated')
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
