273bcb2e6a442c5ac5ad1e5288314cf8e9178099
Bridget Suckling <111847401+bridget-suckling@users.noreply.github.com>
2022-11-22 11:17:44 +1300

    first db test written

--- server/db/db.test.js
+++ server/db/db.test.js
@@ -1,1 +1,16 @@
+const { getAllActivities } = require('./db')
 
+const knex = require('knex')
+const config = require('./knexfile')
+const testDb = knex(config.test)
+
+beforeAll(() => testDb.migrate.latest())
+beforeEach(() => testDb.seed.run())
+
+describe('getAllActivities', () => {
+  it('returns activities from db', () => {
+    return getAllActivities(testDb).then((activities) => {
+      expect(activities).toHaveLength(1)
+    })
+  })
+})
